331b750f2b2a7038d5011d9915123b29ba5cd1c5
Jozef Hollý v17nitro <j2.00ghz@gmail.com>
2015-03-06 21:14:47 +0100

    2014 import

---
+++ 2014/Okres/2/Mnohočleny/Mnohočleny/Module1.vb
@@ -0,0 +1,81 @@
+﻿Imports System.Windows.Forms
+Imports System.IO
+
+Module Module1
+    Dim opf As New OpenFileDialog
+    Dim saf As New SaveFileDialog
+    Dim rr As StreamReader
+    Sub Main()
+        opf.InitialDirectory = "C:\Users\Jozef\Desktop\prog14\mnohocleny"
+        If opf.ShowDialog = DialogResult.OK Then
+            saf.FileName = Path.Combine(Path.GetPathRoot(opf.FileName), "/mnohocleny.dat")
+            If saf.ShowDialog() = DialogResult.OK Then
+                Dim sw As New StreamWriter(saf.FileName)
+                rr = New StreamReader(opf.FileName) 'otevreni souboru a vybrani souboru k ulozeni
+                While Not rr.EndOfStream
+                    Dim radek As String = rr.ReadLine
+                    If radek.Contains(";") And radek.Split(";").Count = 2 And Not radek.Contains(",,") Then 'rozdeleni na jednotlive mnohocleny
+                        Console.Write(radek & " = ")
+                        Dim prvni As New List(Of Integer)
+                        For Each i As String In radek.Split(";")(0).Split(",")
+                            prvni.Add(i)
+                        Next
+                        Dim druhy As New List(Of Integer)
+                        For Each i As String In radek.Split(";")(1).Split(",")      'ulozeni prvniho a druheho mnohoclenu do listu
+                            druhy.Add(i)
+                        Next
+                        Dim vysl As New List(Of List(Of Integer))
+                        For i As Integer = 0 To druhy.Count - 1         'nasobeni prvniho m. kazdym clenem druheho
+                            vysl.Add(posun(prvni, i))                   'pridani nul podle aktualniho nasobice
+                            For x = 0 To vysl.Last.Count - 1
+                                While vysl.Last.Count - 1 < x + i
+                                    vysl.Last.Add(0)
+                                End While
+                                vysl.Last()(x + i) *= druhy(i)          'nasobeni
+                            Next
+                        Next
+                        Dim vysle As New List(Of Integer)               'scitani vysledku
+                        For Each i As List(Of Integer) In vysl
+                            While vysle.Count - 1 < i.Count - 1
+                                vysle.Add(0)
+                            End While
+                            For x = 0 To i.Count - 1
+                                vysle(x) += i(x)
+                            Next
+                        Next
+                        vysle.Reverse()
+                        vysle.RemoveRange(0, druhy.Count - 1)           'odstraneni prebytecnych nul
+                        vysle.Reverse()
+                        For Each i As Integer In vysle
+                            Console.Write(i.ToString & ",")             'zobrazeni
+                            sw.Write(i.ToString & ",")                  'zapsani
+                        Next
+                        sw.WriteLine()
+                        sw.Flush()
+                        Console.WriteLine()
+                    ElseIf radek.Length = 0 Then
+                    Else
+                        Console.WriteLine("Špatný formát vstupu")
+                    End If
+                End While
+            End If
+        End If
+        Console.ReadKey()
+    End Sub
+    ''' <summary>
+    ''' pridava i nul do listu
+    ''' </summary>
+    ''' <param name="list"></param>
+    ''' <param name="i"></param>
+    ''' <returns></returns>
+    ''' <remarks></remarks>
+    Private Function posun(list As List(Of Integer), i As Integer) As List(Of Integer)
+        Dim l As New List(Of Integer)
+        For x = 1 To i
+            l.Add(0)
+        Next
+        l.AddRange(list)
+        Return l
+    End Function
+
+End Module
---
+++ 2014/Okres/3/Sad/Sad/Form1.vb
@@ -0,0 +1,35 @@
+﻿Imports System.IO
+
+Public Class Form1
+    Dim stromy As New List(Of Point)
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        stromy.Clear()
+        Dim s() As String
+        s = TextBox1.Text.Split(Environment.NewLine)        'cteni
+        For Each st As String In s
+            Try
+                stromy.Add(New Point(st.Split(" ")(0), st.Split(" ")(1)))   'pridani souradnic do listu
+            Catch
+            End Try
+
+        Next
+        PictureBox1.Refresh()
+
+    End Sub
+    Private Sub PictureBox1_Paint(sender As Object, e As PaintEventArgs) Handles PictureBox1.Paint
+        For Each p As Point In stromy
+            p.Offset(-1, -1)
+            e.Graphics.FillRectangle(Brushes.Black, New Rectangle(p, New Size(2, 2)))   'vykresleni
+        Next
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If OpenFileDialog1.ShowDialog = Windows.Forms.DialogResult.OK Then
+            Dim sr As New StreamReader(OpenFileDialog1.FileName)    'nasteni ze souboru
+            sr.ReadLine()
+            TextBox1.Text = sr.ReadToEnd
+            sr.Close()
+        End If
+    End Sub
+End Class
