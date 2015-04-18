fbb2d8e2a7960b1dfcf233a596e97b51b1278d79
Jozef Hollý v17nitro <j2.00ghz@gmail.com>
2015-04-18 22:06:10 +0200

    import

---
+++ 2015/skolni/Honba za pokladem/Controls.vb
@@ -0,0 +1,15 @@
+﻿Public Class Controls
+    Event polickoSizeChanged(sender As Object, e As EventArgs)
+    Event pohyb(sender As Object, e As EventArgs)
+    Private Sub Button_Click(sender As Object, e As EventArgs) Handles Button4.Click, Button3.Click, Button2.Click, Button1.Click
+        RaiseEvent pohyb(sender, e)
+    End Sub
+
+    Private Sub NumericUpDown1_ValueChanged(sender As Object, e As EventArgs) Handles NumericUpDown1.ValueChanged
+        RaiseEvent polickoSizeChanged(NumericUpDown1, EventArgs.Empty)
+    End Sub
+
+    Private Sub Controls_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
